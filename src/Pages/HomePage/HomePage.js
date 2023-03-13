06a3d335a850c6f330a167f941b1115b1c33048a
Blubas35 <dtevelis@gmail.com>
2023-03-13 18:07:53 +0200

    homePage done

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,48 +1,109 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Container from '../../Components/container/Container'
+import backgroundImage from '../../Components/Images/hero-box-background.jpg'
 
 const HomePage = () => {
 
+    const [booksData, setBooksData] = useState('')
+    const [authorsData, setAuthorsData] = useState('')
+    const [authorsImage, setAuthorsImage] = useState('')
+    const [reviewsData, setReviewsData] = useState('')
 
+    useEffect(() => {
+        fetch('http://localhost:3000/books/')
+            .then(res => res.json())
+            .then(data => {
+                setBooksData(data)
+            })
 
+    }, [])
+    useEffect(() => {
+        fetch('http://localhost:3000/authors/')
+            .then(res => res.json())
+            .then(data => {
+                setAuthorsData(data)
+                setAuthorsImage(data.map(author => author.image))
+            })
+
+    }, [])
+    useEffect(() => {
+        fetch('http://localhost:3000/reviews?_limit=3')
+            .then(res => res.json())
+            .then(data => {
+                setReviewsData(data)
+            })
+    }, [])
     return (
-        <Container>
-            <div>Hero box background image</div>
-            <div className='left-container'>main content
-                <div className='selling-point-left'>
-                    <h2>Deciding what to read next?</h2>
-                    <p>You’re in the right place. Tell us what titles or genres you’ve enjoyed in the past, and we’ll give you surprisingly insightful recommendations. </p>
-                </div>
-                <div className='selling-point-right'>
-                    <h2>What are your friends reading</h2>
-                    <p>Chances are your friends are discussing and reviewing their favorite (and least favorite) books on WhatToRead. </p>
-                </div>
-                <div className='card-slide-wrapper'>
-                    <div className='card-item'>
-                        <img alt='cia bus book foto'></img>
+        <>
+            <div className='hero-box-image-wrapper'>
+                <div
+                    className='hero-box-background'
+                    style={{
+                        backgroundImage: `url(${backgroundImage})`,
+                        backgroundSize: 'cover',
+                        height: '100vh'
+                    }}
+                ></div>
+            </div>
+            <Container>
+                <div className='left-container'>
+                    <div className='selling-point-left'>
+                        <h2>Deciding what to read next?</h2>
+                        <p>You’re in the right place. Tell us what titles or genres you’ve enjoyed in the past, and we’ll give you surprisingly insightful recommendations. </p>
+                    </div>
+                    <div className='selling-point-right'>
+                        <h2>What are your friends reading</h2>
+                        <p>Chances are your friends are discussing and reviewing their favorite (and least favorite) books on WhatToRead. </p>
+                    </div>
+                    <div className='card-slide-wrapper'>
+                        <h3>What will you discover?</h3>
+                        {booksData && booksData.length > 0 && (
+                            booksData.map((data, index) => (
+                                <div key={index} className='card-item'>
+                                    <img src={data.image} alt='cia bus book foto' width='150'></img>
+                                </div>
+                            ))
+                        )}
+
+                    </div>
+                    <div className='quote-wrapper'>
+                        <h2>Quotes</h2>
+                        <div className='quote-content-wrapper'>
+
+                            <div className='quote-author-image'>
+                                <img src={authorsImage[1]} alt='authors portrait' width='150'></img>
+                            </div>
+                            <div className='quote-text'>
+                                <p>"I am not afraid of storms, for I am learning how to sail my ship."</p>
+                                <span>by Gitanas Nauseda</span>
+                                {/* <span>by Louisa May Alcott</span> */}
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className='quote-wrapper'>
-                    <h2>Quotes</h2>
-                    <div className='quote-content-wrapper'>
-                        <div className='quote-author-image'>
-                            <img alt='cia bus foto'></img>
+                {reviewsData && reviewsData.length > 0 && (
+                    reviewsData.map((data, index) => (
+                        <div key={index} className='right-container'>
+                            <h4>User reviews</h4>
+                            <div className='review-list-wrapper'>
+                                <ul className='review-list'>
+                                    <li className='review-item'>
+                                        <div className='reviewer-image'>
+                                            <img src={data.image} alt='reviewer portrait' width='50'></img>
+                                        </div>
+                                        <div className='review-info-wrapper'>
+                                            <h5>{data.reviewer}</h5>
+                                            <span>{data.rating}/5</span>
+                                            <span>{data.comment}</span>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                        <div className='quote-text'>
-                            <p>Teleloto</p>
-                            <span>by Gitanas Nauseda</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className='right-container'>side content
-                <div className='review-list-wrapper'>
-                    <ul className='review-list'>
-                        <li className='review-item'></li>
-                    </ul>
-                </div>
-            </div>
-        </Container>
+                    ))
+                )}
+            </Container>
+        </>
     )
 }
 
--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -16,18 +16,15 @@ const Search = () => {
         fetch(`http://localhost:3000/books?q=${keyword}`)
             .then(res => res.json())
             .then(results => {
-                console.log(results)
                 setBookSearch(results)
                 setBookTitle(results.map(result => result.title))
             })
     }, [keyword])
-    console.log(bookTitle)
 
     useEffect(() => {
         fetch(`http://localhost:3000/reviews?q=${keyword}`)
             .then(res => res.json())
             .then(results => {
-                console.log(results)
                 setReviewsSearch(results)
             })
     }, [keyword])
@@ -36,95 +33,114 @@ const Search = () => {
         fetch(`http://localhost:3000/authors?q=${keyword}`)
             .then(res => res.json())
             .then(results => {
-                console.log(results)
                 setAuthorsSearch(results)
             })
     }, [keyword])
 
     return (
         <Container>
-            <div className='book-search-wrapper'>
-                <h2>Keyword ({keyword}) found in the book list</h2>
-                <div className='book-result-wrapper'>
-                    {bookSearch && bookSearch.length > 0 && (
-                        bookSearch.map((result, index) => {
-                            return (
-                                    <ul key={index} className='search-list'>
-                                        <li className='search-item'>
-                                            <div className='book-image-wrapper'>
-                                                <img src={result.image} alt='book portrait' width='100'></img>
-                                            </div>
-                                            <div className='book-description-wrapper'>
-                                                <div className='content-wrapper'>
-                                                    <h4>Book title: <Link to={'/book/' + result.id}><span>{result.title}</span></Link></h4>
-                                                    <span>Book author: {result.author}</span>
-                                                    <p>Book description: {result.description}</p>
-                                                </div>
-                                            </div>
-                                        </li>
-                                    </ul>
-                            )
-                        })
-                    )}
-                </div>
-            </div>
+            {keyword && keyword.length > 0 && (
+                <>
+                    <>
+                        {bookSearch && bookSearch.length > 0 && (
+                            <div className='book-search-wrapper'>
+                                <h2>Keyword ({keyword}) found in the book list</h2>
+                                <div className='book-result-wrapper'>
+                                    {bookSearch && bookSearch.length > 0 && (
+                                        bookSearch.map((result, index) => {
+                                            return (
+                                                <ul key={index} className='search-list'>
+                                                    <li className='search-item'>
+                                                        <div className='book-image-wrapper'>
+                                                            <img src={result.image} alt='book portrait' width='100'></img>
+                                                        </div>
+                                                        <div className='book-description-wrapper'>
+                                                            <div className='content-wrapper'>
+                                                                <h4>Book title: <Link to={'/book/' + result.id}><span>{result.title}</span></Link></h4>
+                                                                <span>Book author: {result.author}</span>
+                                                                <p>Book description: {result.description}</p>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                            )
+                                        })
+                                    )}
+                                </div>
+                            </div>
+                        )}
+                    </>
 
-            <div className='reviews-search-wrapper'>
-            <h2>Keyword ({keyword}) found in the review list</h2>
-                <div className='reviews-result-wrapper'>
-                    {reviewsSearch && reviewsSearch.length > 0 && (
-                        reviewsSearch.map((result, index) => {
-                            return (
-                                <ul key={index} className='search-list'>
-                                    <li className='search-item'>
-                                        <div className='review-image-wrapper'>
-                                            <img src={result.image} alt='reviewer portrait' width='100'></img>
-                                        </div>
-                                        <div className='review-description-wrapper'>
-                                            <div className='content-wrapper'>
-                                                <h4>Reviewer: {result.reviewer}</h4>
-                                                <span>User review rating: {result.rating}/5</span>
-                                                <p>User review: <Link to={'/book/' + result.bookId}><span>{result.comment} (click to see the book!)</span></Link> </p>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </ul>
-                            )
-                        })
+                    <>
+                        {reviewsSearch && reviewsSearch.length > 0 && (
 
-                    )}
-                </div>
-            </div>
+                            <div className='reviews-search-wrapper'>
+                                <h2>Keyword ({keyword}) found in the review list</h2>
+                                <div className='reviews-result-wrapper'>
+                                    {reviewsSearch && reviewsSearch.length > 0 && (
+                                        reviewsSearch.map((result, index) => {
+                                            return (
+                                                <ul key={index} className='search-list'>
+                                                    <li className='search-item'>
+                                                        <div className='review-image-wrapper'>
+                                                            <img src={result.image} alt='reviewer portrait' width='100'></img>
+                                                        </div>
+                                                        <div className='review-description-wrapper'>
+                                                            <div className='content-wrapper'>
+                                                                <h4>Reviewer: {result.reviewer}</h4>
+                                                                <span>User review rating: {result.rating}/5</span>
+                                                                <p>User review: <Link to={'/book/' + result.bookId}><span>{result.comment} (click to see the book!)</span></Link> </p>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                            )
+                                        })
 
-            <div className='authors-search-wrapper'>
-                <h2>Keyword ({keyword}) used in the authors list</h2>
-                <div className='author-result-wrapper'>
-                    {authorsSearch && authorsSearch.length > 0 && (
-                        authorsSearch.map((result, index) => {
-                            let firstSentence = result.description.match((/[^\.!\?]+[\.!\?]+/g))
-                            return (
-                                <ul key={index} className='search-list'>
-                                    <li className='search-item'>
-                                        <div className='author-image-wrapper'>
-                                            <img src={result.image} alt='author portrait' width='100'></img>
-                                        </div>
-                                        <div className='author-description-wrapper'>
-                                            <div className='content-wrapper'>
-                                                <h4>Author: <Link to={'/author/' + result.id}><span>{result.name}</span> </Link></h4>
-                                                <span>This author has written books such as: <Link to={'/book/' + result.id}><span>{bookTitle}</span> </Link> </span>
-                                                <p>Short description about author: {firstSentence[0]}</p>
-                                            </div>
-                                        </div>
-                                    </li>
+                                    )}
+                                </div>
+                            </div>
+                        )}
+                    </>
+                    <>
+                        {authorsSearch && authorsSearch.length > 0 && (
 
-                                </ul>
-                            )
-                        })
-                    )}
-                </div>
-            </div>
+                            <div className='authors-search-wrapper'>
+                                <h2>Keyword ({keyword}) found in the authors list</h2>
+                                <div className='author-result-wrapper'>
+                                    {authorsSearch && authorsSearch.length > 0 && (
+                                        authorsSearch.map((result, index) => {
+                                            let firstSentence = result.description.match((/[^\.!\?]+[\.!\?]+/g))
+                                            return (
+                                                <ul key={index} className='search-list'>
+                                                    <li className='search-item'>
+                                                        <div className='author-image-wrapper'>
+                                                            <img src={result.image} alt='author portrait' width='100'></img>
+                                                        </div>
+                                                        <div className='author-description-wrapper'>
+                                                            <div className='content-wrapper'>
+                                                                <h4>Author: <Link to={'/author/' + result.id}><span>{result.name}</span> </Link></h4>
+                                                                <span>This author has written books such as: <Link to={'/book/' + result.id}><span>{bookTitle}</span> </Link> </span>
+                                                                <p>Short description about author: {firstSentence[0]}</p>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+
+                                                </ul>
+                                            )
+                                        })
+                                    )}
+                                </div>
+                            </div>
+                        )}
+                    </>
+                </>
+            )}
+            {bookSearch.length === 0 && reviewsSearch.length === 0 && authorsSearch.length === 0 && (
+                <h2> No results found... </h2>
+            )}
+
         </Container>
-
     )
 }
 
