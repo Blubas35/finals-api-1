a6811099102b1b86e58cfb6dc8bf77de8e8d9e06
Blubas35 <dtevelis@gmail.com>
2023-03-14 11:24:55 +0200

    responsive desing homepage

--- src/Pages/HomePage/Items/QuoteWrapper.js
+++ src/Pages/HomePage/Items/QuoteWrapper.js
@@ -4,11 +4,11 @@ import './QuoteWrapper.scss'
 const QuoteWrapper = ({ authorsImage }) => {
     return (
         <div className='quote-wrapper'>
-            <h2>Quotes</h2>
+            <h2 className='quote-title'>Quotes</h2>
             <div className='quote-content-wrapper'>
 
                 <div className='quote-author-image'>
-                    <img src={authorsImage} alt='authors portrait' width='150'></img>
+                    <img className='quote-author-image' src={authorsImage} alt='authors portrait' width='150'></img>
                 </div>
                 <div className='quote-text'>
                     <p>"I am not afraid of storms, for I am learning how to sail my ship."</p>
