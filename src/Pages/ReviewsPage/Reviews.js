161fa5587ee22702470778a70e841aa0707d63d5
Blubas35 <dtevelis@gmail.com>
2023-03-14 11:54:03 +0200

    fixed bugs

--- src/Pages/ReviewsPage/Reviews.js
+++ src/Pages/ReviewsPage/Reviews.js
@@ -5,6 +5,7 @@ import Container from '../../Components/container/Container'
 const Reviews = () => {
 
     const [reviewsData, setReviewsData] = useState('')
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         fetch('http://localhost:3000/reviews?_expand=book')
@@ -12,28 +13,33 @@ const Reviews = () => {
             .then(data => {
                 setReviewsData(data)
                 console.log(data)
+                setIsLoading(false)
             })
     }, [])
 
     return (
         <Container>
-            <h1>Reviews list</h1>
-            {reviewsData && reviewsData.length > 0 && (
-                reviewsData.map((review, index) => (
-                    <div key={index} className='review-wrapper'>
-                        <ul>
-                            <li>
-                                <h2>{review.reviewer} has recently reviewed this book: <span><Link to= {`/book/${review.book.id}`} >{review.book.title}</Link></span></h2>
-                                <div className='review-content'>
-                                    <span>{review.comment} </span>
-                                    <span>{review.rating}/5</span>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                )
-
-                )
+            {isLoading ? (
+                <div>Loading...</div>
+            ) : (
+                <>
+                    <h1>Reviews list</h1>
+                    {reviewsData && reviewsData.length > 0 && (
+                        reviewsData.map((review, index) => (
+                            <div key={index} className='review-wrapper'>
+                                <ul>
+                                    <li>
+                                        <h2>{review.reviewer} has recently reviewed this book: <span><Link to={`/book/${review.book.id}`} >{review.book.title}</Link></span></h2>
+                                        <div className='review-content'>
+                                            <span>{review.comment} </span>
+                                            <span>{review.rating}/5</span>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        ))
+                    )}
+                </>
             )}
         </Container>
     )
