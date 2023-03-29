c5f24f3216f322bfc77f9ece0e6adfcc8d3826b5
Blubas35 <dtevelis@gmail.com>
2023-03-29 10:25:16 +0300

    fixed logo link

--- src/Pages/MainNavigation/MainNavigation.js
+++ src/Pages/MainNavigation/MainNavigation.js
@@ -1,7 +1,6 @@
-import { NavLink } from "react-router-dom"
+import { Link } from "react-router-dom"
 import './MainNavigation.scss'
 import logo from '../../Components/Images/logo.png'
-import { useState } from "react";
 import TopNavBar from "../../Components/functions/TopNavBar";
 
 const MainNavigation = () => {
@@ -9,19 +8,10 @@ const MainNavigation = () => {
         <div className="navigation-content-wrapper">
             <div className="navigation-wrapper">
                 <div className="navigation-logo-wrapper">
-                    <img className="navigation-logo" src={logo} alt='page logo'></img>
+                    <Link to='/homePage'>
+                        <img className="navigation-logo" src={logo} alt='page logo'></img>
+                    </Link>
                 </div>
-                {/* <ul className={`main-navigation`}>
-                    <NavLink className='item-link' to='/homePage'> <li className="main-navigation-item">
-                        Home page
-                    </li> </NavLink>
-                    <NavLink className='item-link' to='/reviews'> <li className="main-navigation-item">
-                        Reviews page
-                    </li></NavLink>
-                    <NavLink className='item-link' to='/books'> <li className="main-navigation-item">
-                        Books List page
-                    </li> </NavLink>
-                </ul> */}
                 <TopNavBar></TopNavBar>
             </div>
         </div>
